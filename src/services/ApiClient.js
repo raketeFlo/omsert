e03679d06b9c488b76ff06e613477dfae627baeb
Florian Jost <florian.jost89@gmail.com>
2020-02-05 14:52:38 +0100

    feat: switch to default export

--- src/services/ApiClient.js
+++ src/services/ApiClient.js
@@ -1,8 +1,10 @@
-exports.getCountries = async () => {
+const getCountries = async () => {
   try {
     const countries = await fetch(process.env.REACT_APP_API_URL);
     return countries.json();
   } catch (error) {
-    console.error(error);
+    console.error(error); // eslint-disable-line no-console
   }
 };
+
+export default getCountries;
